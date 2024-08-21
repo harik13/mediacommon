ff93f8f5edaa0d98459b91078fbfac36977eb6b0
harik13 <50199545+harik13@users.noreply.github.com>
2024-08-21 11:27:53 +0530

    Update codec_mpeg4_audio.go

--- pkg/formats/fmp4/codec_mpeg4_audio.go
+++ pkg/formats/fmp4/codec_mpeg4_audio.go
@@ -1,7 +1,7 @@
 package fmp4
 
 import (
-	"github.com/bluenviron/mediacommon/pkg/codecs/mpeg4audio"
+	"github.com/harik13/mediacommon/pkg/codecs/mpeg4audio"
 )
 
 // CodecMPEG4Audio is a MPEG-4 Audio codec.
